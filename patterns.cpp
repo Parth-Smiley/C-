a018377ad4fa67b9f9e9ad64bd590ed3f951365e
Parth-Smiley <parthraval2207@gmail.com>
2026-05-09 09:22:27 +0530

    various attern using nested loops

---
+++ patterns.cpp
@@ -0,0 +1,56 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n = 4;
+//     for (int i = 1 ; i <= n ; i++)
+//     {
+//         for (int j = 1 ; j<=n ; j++)     // Square pattern with numbers
+//         {
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+
+// int main()
+// {
+//     int n = 4;
+//     for (int i = 1 ; i <= n ; i++)
+//     {
+//         char a = 'A';
+//         for (int j = 1 ; j<=n ; j++)     // Sqaure pattern with alphabets
+//         {
+//             cout<<a<<" ";
+//             a++;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n = 3;
+//     int a = 1;
+//     for(int i = 1 ; i <= n ; i++)
+//     {
+//         for(int j = 1 ; j <= n ; j++)
+//         {
+//             cout<<a<<" ";
+//             a++;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+#include<iostream>
