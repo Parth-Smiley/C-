d8cb45f0a21d5bb3fdc4789f41554f4987fca0c9
Parth-Smiley <parthraval2207@gmail.com>
2026-05-09 12:45:25 +0530

    all patterns done

--- patterns.cpp
+++ patterns.cpp
@@ -54,3 +54,23 @@ using namespace std;
 // }
 
 #include<iostream>
+using namespace std;
+ int main()
+ {
+    int n = 5;
+    for(int i = 1 ; i <= 5 ; i++)
+    {
+        for(int _ = 1 ; _ <= n-i ; _++)
+            {
+                cout<<" ";
+            }
+        for(int j = 1 ; j <= i ; j++)
+        {
+            
+            cout<<"* ";
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+ }
